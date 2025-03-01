2fea60a870f0d8f01cc954782683aab7ad2fb689
hemant67890 <godarasubhash337@gmail.com>
2025-03-01 16:00:09 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Replied"
 API_ID    = 24495656
 API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "7705076607:AAHPwB2ifTJg2DCPynWRYvleA3IAaKwthwA"
+BOT_TOKEN = ""
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
